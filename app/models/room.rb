943a7e78b0ef0e5dd1ba3adce78a28da857e3694
sebastian argaez <sargaezu@gmail.com>
2019-04-03 17:53:18 -0500

    tercera version

--- app/models/room.rb
+++ app/models/room.rb
@@ -16,8 +16,8 @@ class Room < ActiveRecord::Base
 
 	validates :title, :description, :beds, :guests, :image_url, :price_per_night, presence: true
 	validates :description, length: { maximum: 400 }
-	validates :beds, numericality: {only_integer: true}
-	validates :price_per_night, numericality: {only_integer: true}
-	validates :guests, numericality: {only_integer: true}
+	validates :beds, numericality: { only_integer: true}
+	validates :price_per_night, numericality: { only_integer: true }
+	validates :guests, numericality: { only_integer: true }
 
 end
