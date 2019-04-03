54dbd8275901534b890bfc93489ae31dee9e6d50
sebastian argaez <sargaezu@gmail.com>
2019-04-02 22:19:20 -0500

    ejercicio7

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,9 +11,37 @@ class RoomsController < ApplicationController
     @room = Room.new(room_params)
     if @room.save
       redirect_to rooms_path
+      flash[:notice] = "Su habitación ha sido agregada correctamente"
     else
+      flash[:alert] = "Su habitación, no pudo ser guardada"
       render :new
     end
+  end
+
+  def edit
+  @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to rooms_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+
+    redirect_to rooms_path
+    flash[:alert] = "La habitación ha sido eliminada" 
   end
 
   protected
